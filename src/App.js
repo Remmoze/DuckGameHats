cc0c246d2988a6d0d3ab3a87b3f64e30e7b70e0f
Remmoze <remmoze@gmail.com>
2022-05-13 01:34:19 +0500

    added recoiljs, main.jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from "@mui/material";
-import { Box } from "@mui/system";
+import Main from "./components/Main";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import { useState } from "react";
-import Menu from "./components/Menu";
-import TeamsList from "./components/Team/TeamsList";
-import { createTeam } from "./components/Team/TeamData";
 
 function App() {
-    const [teams, setTeams] = useState([]);
-    const [showbase, setShowBase] = useState(true);
-    const [showDuck, setShowDuck] = useState(true);
-
-    const [showWelcome, setShowWelcome] = useState(true);
-
-    const addTeam = async (imageUrl, file) => {
-        let team = await createTeam(imageUrl, file);
-        setTeams((teams) => [...teams, team]);
-    };
-
-    const deleteTeam = (id) => {
-        setTeams((teams) => teams.filter((team) => team.id !== id));
-    };
-
-    const deleteAll = () => {
-        setTeams([]);
-    };
-
+    const [showWelcome, setShowWelcome] = useState(window.location.hostname !== "localhost");
     return (
         <>
             <Dialog open={showWelcome} onClose={() => setShowWelcome(false)}>
@@ -34,25 +13,7 @@ function App() {
                     <Button onClick={() => setShowWelcome(false)}>ok</Button>
                 </DialogActions>
             </Dialog>
-            <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="flex-start"
-                rowSpacing={2}
-                columnSpacing={2}
-            >
-                <TeamsList {...{ teams, showbase, showDuck }} onDelete={deleteTeam} />
-            </Grid>
-            <Box
-                sx={{
-                    position: "fixed",
-                    top: 15,
-                    right: 15,
-                }}
-            >
-                <Menu {...{ addTeam, deleteAll, showbase, setShowBase, showDuck, setShowDuck }} />
-            </Box>
+            <Main />
         </>
     );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import Menu from "./Menu";
+import TeamsList from "./Team/TeamsList";
+import { createTeam } from "./Team/TeamData";
+
+const Main = () => {
+    const [teams, setTeams] = useState([]);
+    const [showbase, setShowBase] = useState(true);
+    const [showDuck, setShowDuck] = useState(true);
+
+    const addTeam = async (imageUrl, file) => {
+        let team = await createTeam(imageUrl, file);
+        setTeams((teams) => [...teams, team]);
+    };
+
+    const deleteTeam = (id) => {
+        setTeams((teams) => teams.filter((team) => team.id !== id));
+    };
+
+    const deleteAll = () => {
+        setTeams([]);
+    };
+
+    return (
+        <>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="flex-start"
+                rowSpacing={2}
+                columnSpacing={2}
+            >
+                <TeamsList {...{ teams, showbase, showDuck }} onDelete={deleteTeam} />
+            </Grid>
+            <Box
+                sx={{
+                    position: "fixed",
+                    top: 15,
+                    right: 15,
+                }}
+            >
+                <Menu {...{ addTeam, deleteAll, showbase, setShowBase, showDuck, setShowDuck }} />
+            </Box>
+        </>
+    );
+};
+
+export default Main;
