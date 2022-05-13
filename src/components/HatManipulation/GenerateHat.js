3df4eb9e155c62b080be27585f3b10c80d301b7f
Remmoze <remmoze@gmail.com>
2022-05-13 15:56:14 +0500

    .hat Decryption

--- src/components/HatManipulation/GenerateHat.js
+++ src/components/HatManipulation/GenerateHat.js
@@ -1,8 +1,12 @@
 import { encryptHat } from "./HatCipher.js";
 
 const GenerateHatFile = (name, type, imageUrl) => {
-    const data = new Uint8Array(encryptHat(name, type, imageUrl));
+    let hat = encryptHat(name, type, imageUrl);
+
+    const data = new Uint8Array(hat);
     return new Blob([data], { type: "application/octet-stream" });
 };
+
+//todo: generate png file
 
 export { GenerateHatFile };
--- src/components/HatManipulation/HatCipher.js
+++ src/components/HatManipulation/HatCipher.js
@@ -6,7 +6,14 @@ const hatCapeMagicNo = [225, 50, 133, 154, 95, 61, 2, 0];
 
 const generateIV = () => Array.from(Array(16), () => Math.floor(Math.random() * 256));
 const fromString = (str) => str.split("").map((char) => char.charCodeAt(0));
+const toString = (bytes) => bytes.map((byte) => String.fromCharCode(parseInt(byte))).join("");
 const dataURLToBase64 = (dataURL) => dataURL.substring(dataURL.indexOf(",") + 1);
+const base64TodataURL = (base64) => {
+    if (base64.indexOf(",") === -1) {
+        return "data:image/png;base64," + base64;
+    }
+    return base64;
+};
 const getIntBytes = (x) => {
     const bytes = [];
     for (let i = 3; i >= 0; i--) {
@@ -15,6 +22,16 @@ const getIntBytes = (x) => {
     }
     return bytes.reverse();
 };
+
+const bytesToInt = (bytes) => {
+    let x = bytes[3];
+    for (let i = 2; i >= 0; i--) {
+        x <<= 8;
+        x |= bytes[i];
+    }
+    return x;
+};
+
 const padBytes = (data) => {
     let paddedData = [...data];
     let len = paddedData.length;
@@ -35,12 +52,35 @@ const encryptHat = (name, type, imageURL) => {
     data = data.concat(getIntBytes(imageData.length), imageData);
 
     let saveData = [];
-    debugger;
     const iv = generateIV();
     const aesCBC = new aesjs.ModeOfOperation.cbc(key, iv);
     saveData = saveData.concat(getIntBytes(iv.length), iv);
     saveData.push(...aesCBC.encrypt(padBytes(data)));
+
     return saveData;
 };
 
-export { encryptHat };
+const decryptHat = (hatBytes) => {
+    let offset = 0;
+    const ivLength = bytesToInt(hatBytes.slice(0, (offset += 4)));
+    const iv = hatBytes.slice(offset, (offset += ivLength));
+    const aesCBC = new aesjs.ModeOfOperation.cbc(key, iv);
+    const data = Array.from(aesCBC.decrypt(hatBytes.slice(offset)));
+
+    offset = 8; // 0-8 magic
+    const nameLength = data[offset++];
+    const byteName = data.slice(offset, (offset += nameLength));
+    const name = toString(byteName);
+
+    const imageLength = bytesToInt(data.slice(offset, (offset += 4)));
+    const imageData = data.slice(offset, (offset += imageLength));
+
+    const base64 = btoa(toString(imageData));
+
+    return {
+        url: base64TodataURL(base64),
+        name,
+    };
+};
+
+export { encryptHat, decryptHat };
